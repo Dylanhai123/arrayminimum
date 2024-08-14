e753952232d773be15a776a065ff5f55fd48a7d1
Jason Nguyen <jasonmt99@gmail.com>
2024-08-14 12:40:34 -0400

    fourthCommit

---
+++ arrayminimum/src/arrayminimum/arrayminimum.java
@@ -0,0 +1,15 @@
+package arrayminimum;
+
+public class arrayminimum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int [] numbers = {1, 2, 3, 4};
+
+int min = numbers[0];
+for (int i = 0; i < numbers.length;i++) {
+	
+}
+System.out
+	}
+}
